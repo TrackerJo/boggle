3089ae7259a77fc2872851faf12f3872a6eebd54
Nathaniel Kemme Nash <nkemme54@gmail.com>
2023-03-26 13:51:45 -0400

    Fixed routing

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+const root = resolve(__dirname, 'src')
+const outDir = resolve(__dirname, 'dist')
 
 // vite.config.js
 export default defineConfig({
     // config options
-    base: '/boggle/'
+    root,
+    base: '/boggle/',
+    build: {
+      outDir,
+      emptyOutDir: true,
+      input: {
+        index: resolve(root,  'index.html'),
+        AddProblem: resolve(root,  'challenge' ,'index.html'),
+      }
+    }
   })
